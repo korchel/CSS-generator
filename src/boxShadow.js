2b7aef01535d194d316453f13cfc19e540059d4d
Elena Korchuganova <korchel21@gmail.com>
2025-03-02 20:51:52 +0300

    text shadow added

---
+++ src/boxShadow.js
@@ -0,0 +1,9 @@
+import $ from "jquery";
+
+const insideInput = $("#box_shadow_inside");
+const offsetXInput = $("#box_shadow_offset_x");
+const offsetYInput = $("#box_shadow_offset_y");
+const blurRadiusInput = $("#box_shadow_blur_radius");
+const spreadRadiusInput = $("#box_shadow_spread_radius");
+const opacityInput = $("#box_shadow_opacity");
+const colorInput = $("#box_shadow_color");
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./main.css";
+import $ from "jquery";
+
+$(".tabs-wrapper").each(function () {
+  let tabs = $(this);
+  tabs.find(".tab-item").not(":first").hide();
+  tabs.find(".tab").click(function (e) {
+    e.preventDefault();
+    const currentActiveTab = $(".active");
+    const curretTabItem = tabs.find(".tab-item");
+    const tabToOpen = tabs.find(".tab-item").eq($(this).index());
+    if (!$(this).hasClass("active")) {
+      curretTabItem.hide();
+      tabToOpen.show();
+      $(this).addClass("active");
+      currentActiveTab.removeClass("active");
+    }
+  });
+});
